74433fd196fbfe75466978fe978409840ba0d262
Janne Vikstedt <vikstedt.janne@gmail.com>
2022-03-02 15:30:01 +0200

    Leave enemy entities in world on death, to prevent issues with randomized movement

--- client/src/system/death.rs
+++ client/src/system/death.rs
@@ -1,11 +1,24 @@
-use bevy::prelude::{Commands, DespawnRecursiveExt, Entity, Query};
+use bevy::{
+    prelude::{Commands, DespawnRecursiveExt, Entity, Query},
+    sprite::Sprite,
+};
 
-use crate::component::Health;
+use crate::component::{Dynamic, Enemy, EntityType, Health};
 
-pub fn death(mut commands: Commands, health_q: Query<(Entity, &Health)>) {
-    for (entity, health) in health_q.iter() {
+pub fn death(mut commands: Commands, health_q: Query<(Entity, &Health, Option<&Enemy>)>) {
+    for (entity, health, enemy) in health_q.iter() {
         if health.current <= 0 {
-            commands.entity(entity).despawn_recursive();
+            if enemy.is_some() {
+                commands
+                    .entity(entity)
+                    .remove::<EntityType>()
+                    .remove::<Dynamic>()
+                    .remove::<Health>()
+                    .remove::<Sprite>()
+                    .despawn_descendants();
+            } else {
+                commands.entity(entity).despawn_recursive();
+            }
         }
     }
 }
